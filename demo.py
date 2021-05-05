4c424fd68c40a42351f785193b3403f8229767e4
shathri <shathrig@gmail.com>
2021-05-05 10:46:14 +0530

    susu

---
+++ demo.py
@@ -0,0 +1,12 @@
+susu=69
+susu1="you are an idiot"
+susu2=["mango" , "pinapple" , "drumstick"]
+for fru in susu2:
+    print ("i love " +fru)
+
+susu3=input("enter a value = ")
+def add(a,b):
+    return a+b
+
+print("sum= " , add(susu, int(susu3)))    
+print(add(3,5))
